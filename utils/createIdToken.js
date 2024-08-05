07d19cdc6669c0ce7d4e29ad8e9d41b050e2159e
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2024-08-05 13:14:45 +0100

    Add dotenv package

--- utils/createIdToken.js
+++ utils/createIdToken.js
@@ -1,5 +1,6 @@
 const jose = require("jose");
 const { v4: uuidv4 } = require("uuid");
+require('dotenv').config({ path: './env_files/back-secret.env' });
 
 const secret = new TextEncoder().encode(process.env.FAKE_SSO_JWT_SECRET);
 
