2569fceb45123e092c24cc31a22588a6343c41f9
luucvanderzee <luuc_v_d_zee@hotmail.com>
2025-09-30 09:36:51 -0300

    _

--- utils/createIdToken.js
+++ utils/createIdToken.js
@@ -15,6 +15,8 @@ const createIdToken = (profileId) => {
 
   const uid = uuidv4();
 
+  console.log(profile.email)
+
   // https://www.iana.org/assignments/jwt/jwt.xhtml
   return new jose.SignJWT({
     uid,
--- utils/profiles.js
+++ utils/profiles.js
@@ -7,7 +7,7 @@ const PROFILES = [
     has_email: true,
     verified_email: true,
     new_user: true,
-    email: 'marlin_hauck@kiehn.test',
+    email: 'john_doe@example.com',
     gender: "male",
     birthdate: "2000-01-01",
   },
